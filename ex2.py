7265b7ed220828c157b4e6cd0cff6b2d0feafc54
Acheberto <116881496+Acheberto@users.noreply.github.com>
2023-01-18 23:34:11 +0100

    Update ex2.py
    Sigue sin ir asi que lo dejamos y a por el siguente

--- ex2.py
+++ ex2.py
@@ -4,8 +4,10 @@ string = "hola"
 
 
 def vocaleliminar(string, vocals):
-    output = string.replace(str(vocals), "")
+    output = string.replace(str(vocals), "") #no se como hacer que se canvie porque restando las vocales no va xd
     return output;
 
 
 print(vocaleliminar(string, vocals))
+
+
