212f988bbaf31deb17e3c019ae08ef966237b40e
Acheberto <116881496+Acheberto@users.noreply.github.com>
2023-01-18 23:32:45 +0100

    Initial commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+x = 43
+y = 82
+
+#lo intente con papel y sin el pero no me salia  da  estare desentrenado con las mates nose
+#porque lo que necesitaba era que le deise a dos  numeros otro numero igual que se pudiesen dividir y que fuese entero
+#hasta alli bein si no es entero se vuevle al blucque sino break y con el otro igual lo que no sabia la manera de que
+#fuese probando nuemros uno por uno y que los feuse comprobando con unbucle si pero no me salia asi que pase al siguente
+
+
---
+++ ex2.py
@@ -0,0 +1,11 @@
+﻿vocals = ("a", "e", "i", "o", "u")
+
+string = "hola"
+
+
+def vocaleliminar(string, vocals):
+    output = string.replace(str(vocals), "")
+    return output;
+
+
+print(vocaleliminar(string, vocals))
